562758eb56ab9939c54fcf997874d4a37e213df6
bhb192 <accanfield@gmail.com>
2018-09-28 12:13:24 -0700

    0.9.7
    Removed gutenberg, install directly from WP instead

--- opendmo.php
+++ opendmo.php
@@ -6,7 +6,7 @@ Plugin URI: http://www.opendmo.org
 GitHub Plugin URI: https://github.com/bhb192/opendmo
 Description: WordPress Tools for Destination Marketers and Travel Bloggers
 Author: Austin Canfield
-Version: 0.9.6
+Version: 0.9.7
 Author URI: http://www.acanf.pub
 License: GPL2
 */
@@ -25,6 +25,5 @@ foreach (glob($opendmo_path."define/functions/*.php") as $filename) {
 
 }  
 
-include($opendmo_path.'extends/gutenberg/gutenberg.php');
 include($opendmo_path.'extends/pts/pts.php');
 include($opendmo_path.'extends/acf/acf.php');
