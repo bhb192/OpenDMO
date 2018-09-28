a6f23e80c51f6b12ce9527f7a103cee90d73b983
bhb192 <accanfield@gmail.com>
2018-09-28 11:57:08 -0700

    0.9.6
    Revised custom post type supports

--- define/functions/cpt.php
+++ define/functions/cpt.php
@@ -28,8 +28,8 @@ function opendmo_cpt_register() {
            'public'      => true,
            'has_archive' => true,
            'show_in_menu'=>'opendmo-settings',
-            'supports' => array('editor', 'revisions', 'comments'),
-            'show_in_rest' => true,
+           'supports' => array('title', 'editor', 'author', 'thumbnail', 'excerpt'),
+           'show_in_rest' => true,
 
 
 
