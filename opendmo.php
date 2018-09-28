e60432f3ec2a405090eb7b1363ee4f37e6c4fef3
bhb192 <accanfield@gmail.com>
2018-09-28 02:20:13 -0700

    0.9.5
    Updated version to match Github commit name

--- opendmo.php
+++ opendmo.php
@@ -6,7 +6,7 @@ Plugin URI: http://www.opendmo.org
 GitHub Plugin URI: https://github.com/bhb192/opendmo
 Description: WordPress Tools for Destination Marketers and Travel Bloggers
 Author: Austin Canfield
-Version: 1.0
+Version: 0.9.5
 Author URI: http://www.acanf.pub
 License: GPL2
 */
